feeac9dfdead6ca197455f4db4734a2e648ec12a
Wahid Tahosin Laam <laamwahidtahosin@gmail.com>
2025-11-04 10:45:37 +0600

    about not done

--- src/app/components/Description.jsx
+++ src/app/components/Description.jsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import des from "../../../public/images/description.png";
 
 const Description = () => {
   return (
-    <section className='bg-[url("/images/description.png")]'>
-      hello world
+    <section className="flex bg-black justify-between">
+      <div className=" py-14 pl-26">
+        <h3 className="font-mont text-3xl font-bold text-white">IT BERRIES</h3>
+        <p className="max-w-[1150px] font-sans text-base text-white pt-16 pb-6.5">Nulla in velit a metus rhoncus tempus. Nulla congue nulla vel sem varius finibus. Sed ornare sit amet lorem sed viverra. In vel urna quis libero viverra facilisis ut ac est. Morbi commodo, eros in dignissim tempus, lacus odio rutrum augue, in semper sem magna quis tellus. Etiam enim erat, suscipit eu semper a, dictum sit amet elit. Nunc egestas nisi eget enim gravida facilisis. Pellentesque laoreet varius turpis vel pharetra. Ut ante justo, consequat vitae elementum tempor, accumsan nec eros. </p>
+        <Link href="#">|  Read more  |</Link>
+      </div>
+     <div>
+      
+     </div>
     </section>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Description from "./components/Description";
+import About from "./components/About";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
     <Navbar />
     <Banner />
     <Description />
+    <About />
     <Footer />
     </>
   );
